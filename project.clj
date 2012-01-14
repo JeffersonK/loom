7e9b6523b2c4017c534b8ebd5206ae5b70fa2b54
Jeff Kaditz <jeff.kaditz@gmail.com>
2012-01-13 20:13:46 -0800

    added a main and some other project.clj changes.

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
-(defproject jkkramer/loom "0.1.0"
+(defproject jkaditz/loom "0.1.0"
   :description "Graph library for Clojure"
   :author "Justin Kramer"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
-  :jvm-opts ["-Xmx1g" "-server"])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
+                     [lein-ring "0.4.5"]]
+  :jvm-opts ["-Xmx1g" "-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
+  :uberjar-exclusions [#"META-INF.*"]
+  :javac-options {:debug "true" :fork "true"}
+  :aot :all
+  :main loom.core
+  )
